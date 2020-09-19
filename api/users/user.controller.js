68a4f9c7df8ba5b64729a0128e8523b91af6c71a
Amarsh <amartya287@gmail.com>
2020-09-19 19:22:48 +1000

    restructure

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -9,51 +9,42 @@ const {
 module.exports = {
   createAProduct: (req, res) => {
     const body = req.body;
-    // const salt = genSaltSync(10);
-    // body.password = hashSync(body.password, salt);
+    try{
     createAProduct(body, (err, results) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({
-          success: 0,
-          message: "Database connection error"
-        });
-      }
-      return res.status(200).json({
-        success: 1,
-        data: results
-      });
-    });
-  },
-  getProducts: (req, res) => {
-    getProducts((err,results) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({
-          success: 0,
-          message: "Database connection error"
-        });
-      }
-      return res.status(200).json({
-        success: 1,
-        data: results
-      });
-    });
-  },
-  createFromCVS: (req, res) => {
-      createFromCVS( (err, results) => {
-        if (err) {
-          console.log(err);
+     
+        return res.status(200).json({sucess:1, data:results})}); }
+        catch(err) {console.log(err);
           return res.status(500).json({
             success: 0,
             message: "Database connection error"
           });
-        }
-        return res.status(200).json({
-          success: 1,
-          data: results
-        });
-      });
-    }
-  };
+      
+  }},
 
+  getProducts: (req, res) => {
+    try{ 
+      getProducts( (err, results) => {
+      return res.status(200).json({sucess:1, data:results})}); }
+      catch(err) {console.log(err);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error"
+        });
+    
+}
+},
+  
+  createFromCVS: (req, res) => {
+     
+        try{ 
+          createFromCVS( (err, results) => {
+          return res.status(200).json({sucess:1, data:results})}); }
+          catch(err) {console.log(err);
+            return res.status(500).json({
+              success: 0,
+              message: "Database connection error"
+            });
+        
+    }
+  },
+}
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
-require("dotenv").config();
+
+//Getting the modules
 const express = require("express");
 const app = express();
-const userRouter = require("./api/users/user.router");
+// to make api calls 
+const cors = required('cors'); 
+// For our dot environmnet
+const dotenv = required('dotenv');
+dotenv.config();
 
+
+//To Send data to our backend when we get data from our api
+app.use(cors());
+//To be able send it in Json format
 app.use(express.json());
+//To not send any form data
+app.use(express.urlencoded({extended:false}));
+
+
+const userRouter = require("./api/users/user.router");
 
 app.use("/api/users", userRouter);
 const port = process.env.PORT || 4000;
